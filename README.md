f22c3e95d5a7e0ffc69b0f0fdd6b5cfd367e7001
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-04-18 13:20:42 +0800

    feat(wavenet_gcloud.py): introduce Google Wavenet API

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# TTS using iFlyIME Cloud
+# TTS using Cloud Resources
 
 ## Sign up for an API KEY
 
 Go to [XunFei Voice Cloud](http://www.xfyun.cn/) to register a new account and follow the instructions to create a 
-WEBAPI APP first. After that, Enable the TTS service and add your IP to the whitelist.
+WEBAPI APP first. After that, Enable the TTS service and add your IP to the whitelist.  (For Chinese TTS)
+
+Go to [Google Cloud](https://cloud.google.com) to register a TTS API and enable it in Billing Tab. After that, Create a
+Service account key and save it secretly. After that, follow the instructions. (NOTE: You'd better use it in Google
+Compute Engine, because Google Cloud SDK is too sucked, still Python 2.7 ......) For more details, you need to see their
+documents, This program use WaveNet model which is developed under Tensorflow with a better effect.  (For English TTS)
 
 ## Configuration
 
@@ -23,5 +28,5 @@ Follow the instruction shown on your screen.
 
 ## All Done
 
-The output file is ```output.wav``` in your current working directory.
+The output file is ```output.wav``` or ```output.ogg``` in your current working directory.
 The current working directory will be shown at the very beginning of the program running.
---
+++ wavenet_gcloud.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.6
+# -*- encoding: utf-8 -*-
+
+# Auth Token: (Service account) $ gcloud auth application-default print-access-token
+# Set up Gcloud SDK: gcloud auth activate-service-account --key-file=[PATH]
+
+import os
+import base64
+import requests
+
+
+def main():
+    input("Execute: $ gcloud auth activate-service-account --key-file=[PATH] ")
+    apikey = input("Execute: $ gcloud auth application-default print-access-token ")
+    ttstext = input("Text you want to transfer to a speech: ")
+    baseurl = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize'
+    inputdatas = {
+        'input': {'text': ttstext},
+        'voice': {'languageCode': 'en-US', 'name': 'en-US-Wavenet-D', 'ssmlGender': 'MALE'},
+        'audioConfig': {'audioEncoding': 'OGG_OPUS'}
+    }
+    custom_auth = {"Authorization": "Bearer " + str(apikey)}
+    r = requests.post(baseurl, json=inputdatas, headers=custom_auth)
+    json_resp = r.json()
+    voiceb64 = json_resp['audioContent'].encode()
+    decodeddata = base64.b64decode(voiceb64)
+    savedata(decodeddata)
+    print("Done!")
+    return 0
+
+
+def savedata(data):
+    optfile = open(os.path.expanduser('./output.ogg'), 'wb')
+    optfile.write(data)
+    optfile.close()
+    print("File saved at: " + os.path.expanduser('./output.ogg'))
+    return 0
+
+
+if __name__ == '__main__':
+    cwd = os.getcwd()
+    print("Current Working Directory: " + str(cwd))
+    print("Please ensure that you have write permission to this directory.")
+    main()
